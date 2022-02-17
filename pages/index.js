d1e192df5324bb29750134759041eb1f368c0cca
fuck <xtang2828@gmail.com>
2022-02-17 15:36:23 +0800

    complete home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import { hostname } from 'os'
 import Header from "../components/Header"
+import Hero from "../components/Hero"
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header/>
-     <h2>fuck g</h2>
-    </div>
+      <Hero />
+    </>
     
   )
 }
